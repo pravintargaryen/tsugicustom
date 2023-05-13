9113a5fe043f39cc6f7d70c4edf65fb34a22e7b1
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2023-05-13 20:31:32 +0530

    Delete grab.php

--- grab.php
+++
@@ -1,7 +0,0 @@
-<h1>PDO Grab Test Page</h1>
-<html>
-<form method="GET'>
-<p>Enter Email id to fetch message :</p>	
-<input type="text" name="email" size="20">
-<input type="submit" value="Grab"/>
-</html>
