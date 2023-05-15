a5696c5295dc6a2a4149554147ff0017eecac0a7
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2023-05-15 17:41:00 +0530

    Delete customlist.php

--- customlist.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    header('Content-Type: application/json; charset=utf-8');
-    if ( !isset($_SESSION['lessons']) ) $_SESSION['lessons'] = array();
-    echo(json_encode($_SESSION['lessons'], JSON_PRETTY_PRINT));
